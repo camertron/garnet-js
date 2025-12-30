d677c2e2e3b0a9fc48b699b3cdc94cefd95fb3bc
Cameron Dutro <camertron@gmail.com>
2025-12-29 22:46:59 -0800

    Correctly get and set class variables considering lexical scoping

--- src/insns/getclassvariable.ts
+++ src/insns/getclassvariable.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, ExecutionResult } from "../execution_context";
 import Instruction from "../instruction";
-import { Class, ClassClass } from "../runtime";
+import { Class, ClassClass, Qnil } from "../runtime";
+import { ClassFrame, IFrameWithOwner } from "../frame";
 
 export default class GetClassVariable extends Instruction {
     public name: string;
@@ -15,13 +16,47 @@ export default class GetClassVariable extends Instruction {
         let klass = context.frame!.self;
         if (klass.klass !== ClassClass) klass = klass.klass;
 
-        // For singleton classes, class variables are looked up from the attached object
-        // (the class or object that the singleton class belongs to)
-        if (klass.get_data<Class>().is_singleton_class && klass.get_data<Class>().attached_object) {
-            klass = klass.get_data<Class>().attached_object!;
+        // class variables are lexically scoped, so look them up from the owner of the method
+        const frame = context.frame as IFrameWithOwner;
+
+        if (frame.owner) {
+            let target = frame.owner.rval;
+
+            // if the owner is a singleton class, look up from the attached object
+            if (target.klass === ClassClass && target.get_data<Class>().is_singleton_class && target.get_data<Class>().attached_object) {
+                target = target.get_data<Class>().attached_object!;
+            }
+
+            const value = await target.cvar_get(this.name);
+            context.push(value);
+
+            return null;
         }
 
-        context.push(await klass.cvar_get(this.name));
+        // if we're in a ClassFrame (module/class body), use self directly,
+        // but if self is a singleton class, look up from the attached object
+        if (context.frame instanceof ClassFrame) {
+            let target = context.frame.self;
+
+            if (target.klass === ClassClass && target.get_data<Class>().is_singleton_class && target.get_data<Class>().attached_object) {
+                target = target.get_data<Class>().attached_object!;
+            }
+
+            const value = await target.cvar_get(this.name);
+            context.push(value);
+
+            return null;
+        }
+
+        // for singleton classes, class variables are looked up from the singleton class first
+        // (to check extended modules), then from the attached object
+        let value = await klass.cvar_get(this.name);
+
+        if (value === Qnil && klass.get_data<Class>().is_singleton_class && klass.get_data<Class>().attached_object) {
+            value = await klass.get_data<Class>().attached_object!.cvar_get(this.name);
+        }
+
+        context.push(value);
         return null;
     }
 
--- src/insns/setclassvariable.ts
+++ src/insns/setclassvariable.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, ExecutionResult } from "../execution_context";
 import Instruction from "../instruction";
 import { Class, ClassClass } from "../runtime";
+import { ClassFrame, IFrameWithOwner } from "../frame";
 
 export default class SetClassVariable extends Instruction {
     public name: string;
@@ -15,13 +16,54 @@ export default class SetClassVariable extends Instruction {
         let klass = context.frame!.self;
         if (klass.klass !== ClassClass) klass = klass.klass;
 
-        // For singleton classes, class variables are set on the attached object
-        // (the class or object that the singleton class belongs to)
-        if (klass.get_data<Class>().is_singleton_class && klass.get_data<Class>().attached_object) {
-            klass = klass.get_data<Class>().attached_object!;
+        const value = context.pop()!;
+
+        // class variables are lexically scoped, so look them up from the owner of the method
+        const frame = context.frame as IFrameWithOwner;
+
+        if (frame.owner) {
+            let target = frame.owner.rval;
+
+            // if the owner is a singleton class, set on the attached object
+            if (target.klass === ClassClass && target.get_data<Class>().is_singleton_class && target.get_data<Class>().attached_object) {
+                target = target.get_data<Class>().attached_object!;
+            }
+
+            await target.cvar_set(this.name, value);
+
+            return null;
         }
 
-        await klass.cvar_set(this.name, context.pop()!);
+        // if we're in a ClassFrame (module/class body), use self directly,
+        // but if self is a singleton class, set on the attached object
+        if (context.frame instanceof ClassFrame) {
+            let target = context.frame.self;
+
+            if (target.klass === ClassClass && target.get_data<Class>().is_singleton_class && target.get_data<Class>().attached_object) {
+                target = target.get_data<Class>().attached_object!;
+            }
+
+            await target.cvar_set(this.name, value);
+
+            return null;
+        }
+
+        // for singleton classes, check if the class variable exists in the singleton class
+        // (e.g., in extended modules) or in the attached object
+        if (klass.get_data<Class>().is_singleton_class && klass.get_data<Class>().attached_object) {
+            const exists_in_singleton = await klass.cvar_exists(this.name);
+            const exists_in_attached = await klass.get_data<Class>().attached_object!.cvar_exists(this.name);
+
+            if (exists_in_attached && !exists_in_singleton) {
+                await klass.get_data<Class>().attached_object!.cvar_set(this.name, value);
+            } else {
+                // set on the singleton class (will search ancestors including extended modules)
+                await klass.cvar_set(this.name, value);
+            }
+        } else {
+            await klass.cvar_set(this.name, value);
+        }
+
         return null;
     }
 
