4ade2c2cccd62ad105a1619946aebd62391eacdd
Cameron Dutro <camertron@gmail.com>
2026-02-02 22:10:21 -0800

    Refactor the `ENV` constant to make it easier to manipulate from js land

--- src/runtime/env.ts
+++ src/runtime/env.ts
@@ -5,16 +5,44 @@ import { RubyString } from "../runtime/string";
 
 let inited = false;
 
+export class Env {
+    static get instance_rval(): RValue {
+        return ObjectClass.get_data<Class>().constants["ENV"];
+    }
+
+    static get instance(): Env {
+        return this.instance_rval.get_data<Env>();
+    }
+
+    private entries: Hash;
+
+    constructor() {
+        this.entries = new Hash();
+    }
+
+    async get(k: RValue): Promise<RValue> {
+        return await this.entries.get(k);
+    }
+
+    async set(k: RValue, v: RValue) {
+        await this.entries.set(k, v);
+    }
+
+    async has(k: RValue): Promise<boolean> {
+        return await this.entries.has(k);
+    }
+}
+
 export const init = () => {
     if (inited) return;
 
-    const env_hash = new Hash();
-    const env = new RValue(ObjectClass);
+    const env = new Env();
+const env_rval = new RValue(ObjectClass, env);
 
-    env.get_singleton_class().get_data<Class>().define_native_method("[]", async (_self: RValue, args: RValue[]): Promise<RValue> => {
+    env_rval.get_singleton_class().get_data<Class>().define_native_method("[]", async (_self: RValue, args: RValue[]): Promise<RValue> => {
         await Runtime.assert_type(args[0], await RubyString.klass());
 
-        const result = await env_hash.get(args[0]);
+        const result = await env.get(args[0]);
 
         if (is_node && result === Qnil) {
             const result_from_env = process.env[args[0].get_data<string>()];
@@ -29,17 +57,17 @@ export const init = () => {
         return result;
     });
 
-    env.get_singleton_class().get_data<Class>().define_native_method("[]=", async (_self: RValue, args: RValue[]): Promise<RValue> => {
+    env_rval.get_singleton_class().get_data<Class>().define_native_method("[]=", async (_self: RValue, args: RValue[]): Promise<RValue> => {
         await Runtime.assert_type(args[0], await RubyString.klass());
         await Runtime.assert_type(args[1], await RubyString.klass());
-        await env_hash.set(args[0], args[1]);
+        await env.set(args[0], args[1]);
         return args[1];
     });
 
-    env.get_singleton_class().get_data<Class>().define_native_method("include?", async (_self: RValue, args: RValue[]): Promise<RValue> => {
+    env_rval.get_singleton_class().get_data<Class>().define_native_method("include?", async (_self: RValue, args: RValue[]): Promise<RValue> => {
         await Runtime.assert_type(args[0], await RubyString.klass());
 
-        if (await env_hash.has(args[0])) {
+        if (await env.has(args[0])) {
             return Qtrue;
         }
 
@@ -54,7 +82,7 @@ export const init = () => {
         return Qfalse;
     });
 
-    ObjectClass.get_data<Class>().constants["ENV"] = env
+    ObjectClass.get_data<Class>().constants["ENV"] = env_rval;
 
     inited = true;
 };
