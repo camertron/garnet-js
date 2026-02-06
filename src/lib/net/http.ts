23493516b484d211157649bfe815eaa4dd2a4843
Cameron Dutro <camertron@gmail.com>
2026-02-05 18:27:52 -0800

    Use an async initializer for net/http since it uses Runtime.define_class_under (which recently became async itself)

--- src/lib/net/http.ts
+++ src/lib/net/http.ts
@@ -3,14 +3,14 @@ import { Class, ObjectClass, RValue, Runtime } from "../../runtime";
 import { RubyString } from "../../runtime/string";
 import { URI } from "../uri";
 
-export const init = () => {
+export const init = async () => {
     const DEFAULT_PORT: number = 80;
 
     const net_module = Runtime.define_module("Net");
-    const protocol_class = Runtime.define_class_under(net_module, "Protocol", ObjectClass);
-    const response_class = Runtime.define_class_under(net_module, "HTTPResponse", ObjectClass);
+    const protocol_class = await Runtime.define_class_under(net_module, "Protocol", ObjectClass);
+    const response_class = await Runtime.define_class_under(net_module, "HTTPResponse", ObjectClass);
 
-    Runtime.define_class_under(net_module, "HTTP", protocol_class, (klass: Class) => {
+    await Runtime.define_class_under(net_module, "HTTP", protocol_class, (klass: Class) => {
         klass.define_native_singleton_method("get", async (self: RValue, args: RValue[]): Promise<RValue> => {
             const hostname = await Runtime.coerce_to_string(args[0]);
             const path = await Runtime.coerce_to_string(args[1]);
