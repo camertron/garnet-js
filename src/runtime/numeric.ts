146b62a48f723eff4c619ff898eccdb2687f35ab
Cameron Dutro <camertron@gmail.com>
2026-02-28 11:05:49 -0800

    Implement `Numeric#<=>`

--- src/runtime/numeric.ts
+++ src/runtime/numeric.ts
@@ -1,6 +1,8 @@
 import { NameError } from "../errors";
+import { Integer, Qnil } from "../garnet";
 import { Class, ObjectClass, RValue, Runtime } from "../runtime"
 import { Object } from "../runtime/object";
+import { Args } from "./arg-scanner";
 
 export class Numeric {
     private static klass_: RValue;
@@ -21,5 +23,17 @@ export class Numeric {
 export const init = () => {
     Runtime.define_class("Numeric", ObjectClass, async (klass: Class) => {
         klass.include((await Object.find_constant("Comparable"))!);
+
+        // This is just the base implementation of Numeric#<=>. Integer, Float, etc
+        // define more correct versions for those types.
+        klass.define_native_method("<=>", async (self: RValue, args: RValue[]) => {
+            const [other_rval] = await Args.scan("1", args);
+
+            if (self === other_rval) {
+                return await Integer.get(0);
+            }
+
+            return Qnil;
+        });
     });
 }
