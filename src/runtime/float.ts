9f6c0cc8131cbb03201264f5265c0331a8b1871e
Cameron Dutro <camertron@gmail.com>
2026-06-12 15:27:46 -0700

    Float improvements
    1. Coercion
    2. Negation
    3. Pos/neg infinity
    4. Better inspect formatting

--- src/runtime/float.ts
+++ src/runtime/float.ts
@@ -4,12 +4,24 @@ import { Object } from "../runtime/object";
 import { Integer } from "./integer";
 import { Numeric } from "./numeric";
 import { NameError } from "../errors";
+import { Args } from "./arg-scanner";
+import { Kernel } from "./kernel";
+import { RubyArray } from "./array";
+import { Rational } from "./rational";
 
 export class Float {
     private static klass_: RValue;
+    private static POSITIVE_INFINITY: RValue;
+    private static NEGATIVE_INFINITY: RValue;
 
     static async new(value: number): Promise<RValue> {
-        return new RValue(await this.klass(), value);
+        if (value === Number.POSITIVE_INFINITY) {
+            return Float.positive_infinity();
+        } else if (value === Number.NEGATIVE_INFINITY) {
+            return Float.negative_infinity();
+        } else {
+            return new RValue(await this.klass(), value);
+        }
     }
 
     static async klass(): Promise<RValue> {
@@ -23,6 +35,22 @@ export class Float {
 
         return this.klass_;
     }
+
+    static async positive_infinity(): Promise<RValue> {
+        if (!Float.POSITIVE_INFINITY) {
+            Float.POSITIVE_INFINITY = new RValue(await this.klass(), Number.POSITIVE_INFINITY);
+        }
+
+        return Float.POSITIVE_INFINITY;
+    }
+
+    static async negative_infinity(): Promise<RValue> {
+        if (!Float.NEGATIVE_INFINITY) {
+            Float.NEGATIVE_INFINITY = new RValue(await this.klass(), Number.NEGATIVE_INFINITY);
+        }
+
+        return Float.NEGATIVE_INFINITY;
+    }
 }
 
 let inited = false;
@@ -30,9 +58,23 @@ let inited = false;
 export const init = async () => {
     if (inited) return;
 
-    Runtime.define_class("Float", await Numeric.klass(), (klass: Class) => {
+    const float_klass = Runtime.define_class("Float", await Numeric.klass(), (klass: Class) => {
         klass.define_native_method("inspect", async (self: RValue): Promise<RValue> => {
-            return await RubyString.new(self.get_data<number>().toString());
+            let str = self.get_data<number>().toString();
+
+            if (str.indexOf(".") === -1) {
+                // Check if it's in exponential notation (e.g., "5e-20")
+                const eIndex = str.search(/e/i);
+                if (eIndex !== -1) {
+                    // Insert ".0" before the "e"
+                    str = str.substring(0, eIndex) + ".0" + str.substring(eIndex);
+                } else {
+                    // Regular number without decimal point
+                    str = `${str}.0`;
+                }
+            }
+
+            return await RubyString.new(str);
         });
 
         klass.define_native_method("/", async (self: RValue, args: RValue[]): Promise<RValue> => {
@@ -81,10 +123,10 @@ export const init = async () => {
         });
 
         klass.define_native_method("<=>", async (self: RValue, args: RValue[]): Promise<RValue> => {
-            const other = args[0];
+            const [other_rval] = await Args.scan("1", args);
 
-            if (other.klass === await Integer.klass() || other.klass === await Float.klass()) {
-                const other_num = other.get_data<number>();
+            if (other_rval.klass === await Integer.klass() || other_rval.klass === await Float.klass()) {
+                const other_num = other_rval.get_data<number>();
                 const num = self.get_data<number>();
 
                 if (num < other_num) {
@@ -96,7 +138,46 @@ export const init = async () => {
                 }
             }
 
+            if (await Object.respond_to(other_rval, "coerce")) {
+                const coerced = await Object.send(other_rval, "coerce", [self]);
+
+                if (!await Kernel.is_a(coerced, await RubyArray.klass())) {
+                    throw new TypeError("coerce must return [x, y]");
+                }
+
+                const coerced_elems = coerced.get_data<RubyArray>().elements;
+
+                if (coerced_elems.length !== 2) {
+                    throw new TypeError("coerce must return [x, y]");
+                }
+
+                const [coerced_self, coerced_other] = coerced_elems;
+
+                return Object.send(coerced_self, "<=>", [coerced_other]);
+            }
+
             return Qnil;
+        });
+
+        klass.define_native_method("coerce", async (self: RValue, args: RValue[]): Promise<RValue> => {
+            const [other_rval] = await Args.scan("1", args);
+            let other;
+
+            switch (other_rval.klass) {
+                case await Float.klass():
+                    other = other_rval.get_data<number>();
+                    break;
+                case await Rational.klass():
+                    other = other_rval.get_data<Rational>().to_f();
+                    break;
+                case await Integer.klass():
+                    other = other_rval.get_data<number>();
+                    break;
+                default:
+                    throw new TypeError(`${other_rval.klass.get_data<Class>().full_name} can't be coerced into Float`);
+            }
+
+            return RubyArray.new([await Float.new(other), self]);
         });
 
         klass.define_native_method("to_f", (self: RValue): RValue => {
@@ -109,7 +190,18 @@ export const init = async () => {
             if (Number.isInteger(num)) {
                 return await RubyString.new(num.toFixed(1));
             } else {
-                return await RubyString.new(num.toString());
+                let str = num.toString();
+
+                // Handle exponential notation: ensure there's a decimal point before the 'e'
+                if (str.indexOf(".") === -1) {
+                    const eIndex = str.search(/e/i);
+                    if (eIndex !== -1) {
+                        // Insert ".0" before the "e"
+                        str = str.substring(0, eIndex) + ".0" + str.substring(eIndex);
+                    }
+                }
+
+                return await RubyString.new(str);
             }
         });
 
@@ -125,9 +217,15 @@ export const init = async () => {
         });
 
         klass.define_native_method("abs", async (self: RValue): Promise<RValue> => {
-            return await Integer.get(Math.abs(self.get_data<number>()));
+            return await Float.new(Math.abs(self.get_data<number>()));
+        });
+
+        klass.define_native_method("-@", async (self: RValue): Promise<RValue> => {
+            return await Float.new(-self.get_data<number>());
         });
     });
+
+    float_klass.get_data<Class>().constants["INFINITY"] = await Float.positive_infinity();
 
     inited = true;
 };
