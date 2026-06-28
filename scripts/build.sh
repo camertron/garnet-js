c96a325107ac06c7649d0b9a917b9fcdac6f5b1e
Cameron Dutro <camertron@gmail.com>
2026-06-27 22:23:05 -0700

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+npx tsc
+
 if [[ "$1" == "--release" ]]; then
     echo "Building in release mode"
-    export RELEASE=1
+    npx vite build --minify terser
 else
     echo "Building in debug mode"
+    npx vite build
 fi
 
-npx tsc && rollup -c
 brotli -f -q 11 -o dist/garnet.js.br dist/garnet.js
 gzip -9 -c dist/garnet.js > dist/garnet.js.gz
+ls -lah dist | grep garnet.js
